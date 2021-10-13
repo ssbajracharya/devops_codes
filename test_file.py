7c09920f13fbf8d20e8b59b0095aeb1199703f1c
ssbajracharya <ssbajracharya@outlook.com>
2021-10-12 19:35:36 -0500

    Second Commit with chage to test_file.py

--- test_file.py
+++ test_file.py
@@ -1,2 +1,9 @@
 # Just a test file 
-# Nothing to see here 
+
+# Leap Year
+year = int(input("Enter year\n"))
+
+if((year % 400 == 0) or (year % 100 != 0) and (year % 4 == 0)):   
+    print("Leap Year") 
+else:  
+    print ("Not Leap Year")  
