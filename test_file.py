5bd60ee622fe20c8550bad256ca453785840aa33
ssbajracharya <ssbajracharya@outlook.com>
2021-10-12 20:02:19 -0500

    Vouwels count

--- test_file.py
+++ test_file.py
@@ -1,6 +1,10 @@
-# Just a test file 
+# Finds vowels and consants
 
 words = input("Please enter a word \n")
 
+count = 0
+
 for word in words:
-    print(word)
+    if word in "aeiou":
+        count += 1
+print(count)
